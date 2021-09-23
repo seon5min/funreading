177380dfebc378eeb82f255ec0df9fb834e0cccd
seonomin <seon5.min@gmail.com>
2021-09-23 21:43:59 +0900

    23

--- funreading/js/fun_new.js
+++ funreading/js/fun_new.js
@@ -8,36 +8,49 @@ $(function(){
       disableOnInteraction: true
     },
     loop: true,
-    allowTouchMove:false,
-    observer: true,	// 추가
-    observeParents: true,	// 추가
+    allowTouchMove: false,
+    // observer: true,	// 추가
+    // observeParents: true,	// 추가    
   });
-
+  $(window).on('resize', function() {
+    $(".book_topten .swiper-wrapper").css("transform", "translate3d(0px, -33px, 0px)");
+  });
 
   $(".topten .ico_arrow").on('click', function (e) {
     $('.topten').addClass('list_open');
-    rankSwiper.slideTo(1,0,false);
-    // $(".swiper-wrapper").css("transform", "translate3d(0px, -34px, 0px)");
+    rankSwiper.slideTo(1, 0, false);
+    // $(".swiper-wrapper").css("transform", "translate3d(0px, -33px, 0px)");
+    
+    // $(".swiper-slide").css("height", "33px");
+    rankSwiper.autoplay.stop();
+    return false;
+  });
+  $(".book_topten .m_ico_arrow").on('click', function (e) {
+    $('.topten').addClass('list_open');
+    $('.recommend_wrap .top_container').addClass('triangle');
+    $(".book_topten .m_ico_arrow").stop().css({ 'transform': 'rotate(180deg)' }, 5000);
+    rankSwiper.slideTo(1, 0, false);
     rankSwiper.autoplay.stop();
     return false;
   });
 
   $(".rank_list_close").on('click', function (e) {
     $('.topten').removeClass('list_open');
-      rankSwiper.slideTo(1,0,false);
-      rankSwiper.autoplay.start();
+    $('.recommend_wrap .top_container').removeClass('triangle');
+    $(".book_topten .m_ico_arrow").stop().css({'transform': 'rotate(360deg)',"top":"0px"},1000);
+    rankSwiper.slideTo(1,0,false);
+    rankSwiper.autoplay.start();
+
       return false;
   });
 
-  // 카테고리 TAB
-  $('.tab .tabs a').on('click', function (e) { 
-    var idx = $(this).parent().index(); 
-    var $this = $(this);
-    $(this).parent().siblings().removeClass('on'); 
-    $(this).parent().addClass('on'); 
-    $(this).parent().parent().parent().siblings('.tab_con').removeClass('on');
-    $(this).parent().parent().parent().siblings('.tab_con').eq(idx).addClass('on');
-  });
+    // 탭 
+    $('.tabs li').on('click', function (e) {
+      e.preventDefault();
+      $('.tabs li').removeClass('on');
+      $(this).addClass('on');
+    });
+  
 
   // 인기순, 최신순 
   $('.filter-box .order > div').on('click', function (e) {
@@ -46,17 +59,15 @@ $(function(){
     $(this).addClass('on');
   });
 
+
   //Swiper JS - MAIN SLIDER
   var galleryThumbsMain = new Swiper('.gallery_thumbs', {
     spaceBetween: 5,
     slidesPerView: 6,
     loop: true,
-    autoplay: false,
-    observer: true,
-		observeParents: true,
-    touchRatio: 0.2,
+    touchRatio: 0.3,
     slideToClickedSlide: true,
-    freeMode: true,
+    freeMode: false,
     loopedSlides: 6, //looped slides should be the same
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
@@ -70,13 +81,10 @@ $(function(){
   var galleryTopMain = new Swiper('.gallery_top', {
     spaceBetween: 5,
     effect: 'fade',
-    loop: true,
-    observer: true,
-		observeParents: true,
-    // autoplay: false,
+    loop:true,
     autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
+        delay: 3000,
+        disableOnInteraction: false,
     },
     loopedSlides: 6,
     pagination: {
@@ -97,7 +105,7 @@ $(function(){
   galleryTopMain.controller.control = galleryThumbsMain;
   galleryThumbsMain.controller.control = galleryTopMain;
 
-
+//시리즈 슬라이드
   var seriesBookCont = new Swiper(".series_book_cont", {
     slidesPerView: 4, 
     spaceBetween: 30,
@@ -111,12 +119,33 @@ $(function(){
     pagination: {
       el: '.control .progressbar',
       type: 'progressbar',
+      renderProgressbar: function (progressbarFillClass) {
+        return '<span class="' + progressbarFillClass + '"></span>';
+    }
     },
     navigation: {
       nextEl: '.sl-nav .swiper-button-next',
       prevEl: '.sl-nav .swiper-button-prev',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      1140: {
+
+      },
+      768: {
+        slidesPerView: "auto",
+        spaceBetween: 18,
+        touchRatio: 0.3,
+        slideToClickedSlide: true,
+        freeMode: false,
+      }
     }
+    
   });
+
+
+
+
   
   seriesBookCont.on('reachEnd', function() {
     console.log("reachEnd");
@@ -124,4 +153,22 @@ $(function(){
 
   var numOfSlides = document.querySelectorAll(".swiper-slide").length;
   console.log("numOfSlides: " + numOfSlides);/* 3 */
+
+  // 모달창
+
+  //modal 
+$('.series_more_btn').on('click', function (e) {
+  e.preventDefault();
+  $('.modal').addClass('modal-opened');
+  $('body').addClass('bodyscroll');
+});
+$('.btn-modal-close').on('click', function (e) {
+  var body = document.body;
+  e.preventDefault();
+  $('.modal').removeClass('modal-opened');
+  $(body).removeClass('bodyscroll');
+});
+
+
+
 });
